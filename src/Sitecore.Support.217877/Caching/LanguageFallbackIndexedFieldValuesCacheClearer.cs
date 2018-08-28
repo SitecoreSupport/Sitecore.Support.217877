c0aa2fcc9c0797d24d9a31be44d5527b2ad12f49
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-08-28 11:04:33 +0300

    clear language fallback field values cache on item:saved, item:deleted and item:versionremoved remote events

---
+++ src/Sitecore.Support.217877/Caching/LanguageFallbackIndexedFieldValuesCacheClearer.cs
@@ -0,0 +1,46 @@
+﻿namespace Sitecore.Support.Caching
+{
+  using Sitecore.Caching;
+  using Sitecore.Data.Events;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using System;
+  using System.Reflection;
+
+  public class LanguageFallbackIndexedFieldValuesCacheClearer
+  {
+    public void ClearLanguageFallbackCache(object sender, EventArgs args)
+    {
+      try
+      {
+        Item item = null;
+
+        if (args is ItemSavedRemoteEventArgs)
+        {
+          item = ((ItemSavedRemoteEventArgs)args).Item;
+        }
+
+        else if (args is ItemDeletedRemoteEventArgs)
+        {
+          item = ((ItemDeletedRemoteEventArgs)args).Item;
+        }
+
+        else if (args is VersionRemovedRemoteEventArgs)
+        {
+          item = ((VersionRemovedRemoteEventArgs)args).Item;
+        }
+
+        if (item != null && item.Database != null && item.Database.Caches != null && item.Database.Caches.FallbackFieldValuesCache != null)
+        {
+          LanguageFallbackIndexedFieldValuesCache cache = item.Database.Caches.FallbackFieldValuesCache as LanguageFallbackIndexedFieldValuesCache;
+          MethodInfo ClearCacheMethod = typeof(LanguageFallbackIndexedFieldValuesCache).GetMethod("ClearCache", BindingFlags.NonPublic | BindingFlags.Instance);
+          ClearCacheMethod.Invoke(cache, new object[] { item });
+        }
+      }
+      catch
+      {
+        Log.Warn("There was an error while clearing LanguageFallbackIndexedFieldValuesCache", this);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.217877/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.217877")]
+[assembly: AssemblyProduct("Sitecore.Support.217877")]
+[assembly: ComVisible(false)]
